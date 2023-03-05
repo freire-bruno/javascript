863ded85c3bafda97b52c4cbea6fa9ba407de720
Bruno Freire <123323785+freire-bruno@users.noreply.github.com>
2023-03-05 13:11:40 -0300

    Updated clock.

--- javascript-cv/aula12ex/ex013/script.js
+++ javascript-cv/aula12ex/ex013/script.js
@@ -3,7 +3,9 @@ function carregar() {
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
+var minuto = data.getMinutes()
+var segundo = data.getSeconds()
+msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo} horas.`
 if (hora >= 0 && hora < 12) {
     img.src = 'manha.jpeg'
     document.body.style.background = '#e2cd9f'
