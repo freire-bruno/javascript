11e0d2ae9063d9e47962156b877a75aca3f25deb
Bruno Freire <123323785+freire-bruno@users.noreply.github.com>
2023-04-23 13:38:49 -0300

    created new functions ex.

---
+++ javascript-cv/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2==0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = parimpar(4) 
+console.log(res)
+
+/* Ou poderia apenas colocar o "console.log(parimpar(4))"
+*/
---
+++ javascript-cv/aula16/funcao02.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2))
+
+// o 0 acima é um parâmetro opcional, que caso falte algum número no parâmetro inicial, não dê erro.
